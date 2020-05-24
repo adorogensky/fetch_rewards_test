b034a82288e8e1c0fccc2532085566a260da3d9a
Alex Dorogensky <amazinglifetime@gmail.com>
2020-05-24 11:11:09 -0500

    Add start.spring.io generated gradle project

---
+++ src/main/java/com/exebar/fetch_rewards/uniqueemailtest/UniqueEmailTestApplication.java
@@ -0,0 +1,13 @@
+package com.exebar.fetch_rewards.uniqueemailtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqueEmailTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniqueEmailTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exebar/fetch_rewards/uniqueemailtest/UniqueEmailTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exebar.fetch_rewards.uniqueemailtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniqueEmailTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
