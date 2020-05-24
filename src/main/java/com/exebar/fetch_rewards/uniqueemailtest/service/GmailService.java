6a9744eff0f6f6a5df92295cf15caf3c992bdcf2
Alex Dorogensky <amazinglifetime@gmail.com>
2020-05-24 12:48:19 -0500

    GmailServiceTests, GmailService: add more tests and finalize implementation

--- src/main/java/com/exebar/fetch_rewards/uniqueemailtest/service/GmailService.java
+++ src/main/java/com/exebar/fetch_rewards/uniqueemailtest/service/GmailService.java
@@ -3,11 +3,26 @@ package com.exebar.fetch_rewards.uniqueemailtest.service;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class GmailService {
 
     public int getUniqueEmailCount(List<String> emails) {
-        return emails.size();
+        if (emails == null || emails.size() == 0) return 0;
+        if (emails.size() == 1) return 1;
+
+        return emails.stream().map(
+                email -> email.substring(0, email.indexOf('@'))
+        ).map(
+                email -> email.replace(".", "")
+        ).map(
+                this::removeAllAfterPlus
+        ).collect(Collectors.toSet()).size();
+    }
+
+    private String removeAllAfterPlus(String in) {
+        int indexOfPlus = in.indexOf("+");
+        return indexOfPlus != -1 ? in.substring(0, indexOfPlus) : in;
     }
 }
--- src/test/java/com/exebar/fetch_rewards/uniqueemailtest/service/GmailServiceTests.java
+++ src/test/java/com/exebar/fetch_rewards/uniqueemailtest/service/GmailServiceTests.java
@@ -2,16 +2,47 @@ package com.exebar.fetch_rewards.uniqueemailtest.service;
 
 import org.junit.jupiter.api.Test;
 
+import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GmailServiceTests {
 
-    GmailService gmailService = new GmailService();
+    private GmailService gmailService = new GmailService();
 
     @Test
     void oneEmail_getUniqueEmailCount_shoulReturn_1() {
         assertEquals(1, gmailService.getUniqueEmailCount(singletonList("test.email@gmail.com")));
+    }
+
+    @Test
+    void twoEqualEmails_getUniqueEmailCount_shoulReturn_1() {
+        assertEquals(1, gmailService.getUniqueEmailCount(asList("test.email@gmail.com", "test.email@gmail.com")));
+    }
+
+    @Test
+    void oneEmailWithDotAndOneEqualNormalizedEmail_getUniqueEmailCount_shoulReturn_1() {
+        assertEquals(1, gmailService.getUniqueEmailCount(asList("test.email@gmail.com", "testemail@gmail.com")));
+    }
+
+    @Test
+    void oneEmailWithPlusAndOneEqualNormalizedEmail_getUniqueEmailCount_shoulReturn_1() {
+        assertEquals(1, gmailService.getUniqueEmailCount(asList("testemail+spam@gmail.com", "testemail@gmail.com")));
+    }
+
+    @Test
+    void oneEmailWithDotAndOneEqualEmailWithPlusAndOneEqualNormalizedEmail_getUniqueEmailCount_shoulReturn_1() {
+        assertEquals(1, gmailService.getUniqueEmailCount(asList("test.email@gmail.com", "testemail+spam@gmail.com", "testemail@gmail.com")));
+    }
+
+    @Test
+    void oneEmailWithDotAndPlusAndOneEqualNormalizedEmail_getUniqueEmailCount_shoulReturn_1() {
+        assertEquals(1, gmailService.getUniqueEmailCount(asList("test.email+spam@gmail.com", "testemail@gmail.com")));
+    }
+
+    @Test
+    void oneEmailWithDotAndPlusAndOneEqualNormalizedEmailAndAnotherEmail_getUniqueEmailCount_shoulReturn_2() {
+        assertEquals(2, gmailService.getUniqueEmailCount(asList("test.email+spam@gmail.com", "testemail@gmail.com", "test2.email@gmail.com")));
     }
 }
